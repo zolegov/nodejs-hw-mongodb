64836f0eedac07dfd6e007f93c6bdd810b226689
Oleg <zazubik78@gmail.com>
2024-10-05 10:06:23 +0300

    swagger ready get query

---
+++ src/middlewares/swaggerDocs.js
@@ -0,0 +1,14 @@
+import { SWAGGER_PATH } from '../constants/index.js';
+import swaggerUI from 'swagger-ui-express';
+import fs from 'node:fs';
+import createHttpError from 'http-errors';
+
+export const swaggerDocs = () => {
+  try {
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+  } catch (err) {
+    return (req, res, next) =>
+      next(createHttpError(500, "Can't load swagger docs"));
+  }
+};
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
@@ -28,6 +29,7 @@ export const setupServer = () => {
     });
   });
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(router);
   app.use('*', notFoundHandler);
   app.use(errorHandler);
